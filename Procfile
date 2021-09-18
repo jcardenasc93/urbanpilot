98ef0326b5af466524c762588690936fa65f026e
Juan Camilo Cardenas <jcardenasc93@gmail.com>
2021-09-18 17:42:26 -0500

    Fix celery related error on heroku

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python -m flask db upgrade
 web: gunicorn -w 4 wsgi:app
-celeryworker: celery -A celery_worker worker --loglevel=info
+worker: python -m celery -A celery_worker worker --loglevel=info
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 """ Entrypoint for gunicorn app deployment """
 from app import create_app
+from app.tasks.celery_setup import make_celery
 
 app = create_app()
+celery = make_celery(app)
+app.celery = celery
